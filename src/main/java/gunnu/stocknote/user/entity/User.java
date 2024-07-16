c1062566b95ca53c4eb10cb99a50fba68c94c520
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-07-16 17:34:25 +0900

    feat: add userRole and userEntity

---
+++ src/main/java/gunnu/stocknote/user/entity/User.java
@@ -0,0 +1,38 @@
+package gunnu.stocknote.user.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "users")
+@SQLRestriction("deleted_at is NULL")
+@SQLDelete(sql = "UPDATE artists SET deleted_at = NOW() WHERE user_id = ?")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "profile")
+    private String profile;
+
+    @Column(name = "user_role")
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+}
