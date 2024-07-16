f9cca72341956ee7748685dd03c0af96a5861228
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-07-16 17:10:32 +0900

    feat: add BaseEntity

---
+++ src/main/java/gunnu/stocknote/common/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package gunnu.stocknote.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AutoCloseable.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
